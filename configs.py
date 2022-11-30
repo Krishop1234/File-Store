26a035a95684fbdac86cebbffd428371d0f9f42f
Sadula Rushidhar <113742964+Sadula-Rushidhar@users.noreply.github.com>
2022-11-30 15:09:56 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -14,8 +14,8 @@ class Config(object):
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "Use_Full_Bots")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001771518958")
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
-	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
-	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
+	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", False))
+	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 	ABOUT_BOT_TEXT = f"""
