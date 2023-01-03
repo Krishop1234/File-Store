ca76f1f7e96fc072776cd72a628b769e59ab38ed
Sadula Rushidhar <115465446+S-Rushidhar-1-1999@users.noreply.github.com>
2023-01-03 14:36:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/File_store_Rushidhar_Bot')
 def hello_world():
-    return 'Rushidhar1999'
-
+    return redirect("https://telegram.me/File_store_Rushidhar_1Bot", code=302)
 
 if __name__ == "__main__":
     app.run()
