f798678081c3ac32d21e6df7dd025518354106fc
Sadula Rushidhar <115465446+S-Rushidhar-1-1999@users.noreply.github.com>
2023-08-21 12:22:36 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -6,7 +6,11 @@ from pyrogram import Client
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 from handlers.helpers import str_to_b64
+import threading
 
+async def auto_delete_thread(msg):
+    asyncio.sleep(300)
+    return await bot.delete_messages(msg.chat.id, msg.id)
 
 async def media_forward(bot: Client, user_id: int, file_id: int):
     try:
@@ -24,3 +28,7 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
     await asyncio.sleep(2)
+    delete = threading.Thread(
+        target=lambda: asyncio.run(auto_delete_thread(sent_message))
+    )
+    delete.start()
