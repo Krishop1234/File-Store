f40319d5a5a3c64e3f40863eddbf43e4e0214f65
Sadula Rushidhar <115465446+S-Rushidhar-1-1999@users.noreply.github.com>
2023-08-22 16:23:02 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -1,16 +1,20 @@
-# (c) @AbirHasan2005 | @PredatorHackerzZ
-
 import asyncio
 from configs import Config
 from pyrogram import Client
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 from handlers.helpers import str_to_b64
-import threading
 
-async def auto_delete_thread(bot: Client, msg: Message):
-    asyncio.sleep(300)
-    return await bot.delete_messages(msg.chat.id, msg.id)
+
+async def reply_forward(message: Message, file_id: int):
+    try:
+        await message.reply_text(
+            f"**Files will be Deleted After 30 min ⏰**\n",
+            disable_web_page_preview=True, quote=True)
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
+        await reply_forward(message, file_id)
+
 
 async def media_forward(bot: Client, user_id: int, file_id: int):
     try:
@@ -25,10 +29,11 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
         return media_forward(bot, user_id, file_id)
 
 
+async def delete_file(file_id: int):
+    await asyncio.sleep(300)  # wait for 30 minutes
+
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
+    await reply_forward(message=sent_message, file_id=file_id)
+    asyncio.create_task(delete_file(file_id))  # schedule the file deletion task
     await asyncio.sleep(2)
-    delete = threading.Thread(
-        target=lambda: asyncio.run(auto_delete_thread(sent_message))
-    )
-    delete.start()
